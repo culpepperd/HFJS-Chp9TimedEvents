6d64820336880064891fdb888d6507884a92fea1
culpepperd <culpepperd2490@gmail.com>
2023-08-16 23:52:03 -0400

    Testing

--- chp9TE.js
+++ chp9TE.js
@@ -12,11 +12,10 @@ function ticker() {
 
 setInterval(ticker, 1000);
 
-/*
+
 function timerHandler() {
     alert("Hey! What are you doing just sitting there staring at a blank screen?");
     setTimeout(timerHandler,1000);
 }
 
 setTimeout(timerHandler,1000);
-*/
