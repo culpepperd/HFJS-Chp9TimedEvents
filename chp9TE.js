53954aa8943b5f70fb0897e763b131718d1ebd58
culpepperd <culpepperd2490@gmail.com>
2023-08-17 14:08:14 -0400

    testing out clearInterval()

--- chp9TE.js
+++ chp9TE.js
@@ -10,12 +10,32 @@ function ticker() {
     }
 }
 
-setInterval(ticker, 1000);
+var t = setInterval(ticker, 1000);
 
+setTimeout(timerHandler, 5000);
 
+function timerHandler() {
+    // var t = setInterval(ticker, 1000);
+    clearInterval(t);
+}
+/*
+function tickCount(){
+    for(var i = 0; i < 10; i++) {
+        if(i < 10){
+            
+        } else {
+            var t = setInterval(ticker, 1000);
+            clearInterval(t);
+        }
+    }
+}
+*/
+
+/*
 function timerHandler() {
     alert("Hey! What are you doing just sitting there staring at a blank screen?");
     setTimeout(timerHandler,1000);
 }
 
 setTimeout(timerHandler,1000);
+*/
